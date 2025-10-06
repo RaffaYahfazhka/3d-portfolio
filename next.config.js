01681b7558bd594a66801c270038c33e20791c35
muhraffa <raffa@actionpay.co.id>
2025-10-06 22:31:33 +0700

    update config next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 // next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'nama-repo-kamu'; // ganti jika pakai project page
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  poweredByHeader: false,
+  output: 'export',
+  images: { unoptimized: true },
+  // kalau BUKAN username.github.io (project page), aktifkan 2 baris ini:
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  trailingSlash: true,
 
+  // rule shader yang kamu butuhkan kemarin
   webpack: (config) => {
-    // Jadikan file shader sebagai string (asset/source = raw)
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/i,
       type: 'asset/source',
