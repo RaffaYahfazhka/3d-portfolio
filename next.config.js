6a42988c181ec829c7b4e522efc8c707bffc589e
muhraffa <raffa@actionpay.co.id>
2025-10-06 22:39:45 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 // next.config.js
 const isProd = process.env.NODE_ENV === 'production';
-const repo = '3d-portfolio'; // ganti jika pakai project page
+const repo = '3d-portfolio'; 
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  output: 'export',
+  output: 'export',         
   images: { unoptimized: true },
-  // kalau BUKAN username.github.io (project page), aktifkan 2 baris ini:
   basePath: isProd ? `/${repo}` : '',
   assetPrefix: isProd ? `/${repo}/` : '',
   trailingSlash: true,
-
-  // rule shader yang kamu butuhkan kemarin
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/i,
@@ -22,4 +19,3 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
